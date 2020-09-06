7c6c1ed9a6824a3e16dd03eaf32353d5243535c9
FunsomMars <mss1402884667@gmail.com>
2020-09-07 01:55:44 +0800

    Update ui look

--- gui/window.py
+++ gui/window.py
@@ -23,14 +23,17 @@ class Ui_MainWindow(object):
         MainWindow.setSizePolicy(sizePolicy)
         MainWindow.setMinimumSize(QtCore.QSize(500, 500))
         MainWindow.setMaximumSize(QtCore.QSize(500, 500))
-        MainWindow.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
+        font = QtGui.QFont()
+        font.setFamily("微软雅黑")
+        MainWindow.setFont(font)
+        MainWindow.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap("icon-1-1024x1024.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         MainWindow.setWindowIcon(icon)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(160, 150, 160, 41))
+        self.pushButton.setGeometry(QtCore.QRect(160, 155, 160, 41))
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -152,7 +155,7 @@ class Ui_MainWindow(object):
         self.label_3.setAlignment(QtCore.Qt.AlignCenter)
         self.label_3.setObjectName("label_3")
         self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
-        self.textBrowser.setGeometry(QtCore.QRect(40, 210, 420, 241))
+        self.textBrowser.setGeometry(QtCore.QRect(40, 210, 420, 240))
         self.textBrowser.setObjectName("textBrowser")
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -164,7 +167,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "测试图片生成器"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Timg"))
         self.pushButton.setText(_translate("MainWindow", "生成图片"))
         self.label_5.setText(_translate("MainWindow", "图片PPI"))
         self.label.setText(_translate("MainWindow", "图片类型"))
@@ -182,15 +185,15 @@ class Ui_MainWindow(object):
         self.textBrowser.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'.AppleSystemUIFont\'; font-size:13pt; font-weight:400; font-style:normal;\">\n"
+"</style></head><body style=\" font-family:\'微软雅黑\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'.AppleSystemUIFont\'; font-size:18pt; font-weight:600;\">使用手册：</span></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'.AppleSystemUIFont\';\">-----------------------------------------------------------------</span></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'.AppleSystemUIFont\';\">1. 输入所需图片的高度（单位像素）、宽度（单位像素）、储存文件夹的名称，选择想要生成的图片类型，点击生成，就可以在指定文件夹里面生成所需图片；</span></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'.AppleSystemUIFont\';\">2. 本程序支持生成的图片类型有十字对齐、白红绿蓝255灰阶单色、Crosstalk、Flicker、灰阶响应时间动图、棋盘格以及指定的多灰阶白红绿蓝图片；</span></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'.AppleSystemUIFont\';\">3. 灰阶响应时间动图的默认灰阶设置为(0, 36, 63, 127, 255)五个灰阶中从低灰阶到高灰阶跳变，跳变时间间隔为0.1s，也就是说会生成0-36，0-63，0-127，0-255，36-63，36-127，36-255，63-127，63-255，127-255十种灰阶跳变动图；</span></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'.AppleSystemUIFont\';\">4. 黑白棋盘格中子格子的长宽比例为图片宽高比乘以16/9；</span></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'.AppleSystemUIFont\';\">5. 图片生成类型选择为crosstalk时，图片PPI的默认值为401ppi，一般来说在生成crosstalk图片时需要在图片PPI那一栏指定ppi；</span></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'.AppleSystemUIFont\';\">6. 多灰阶白红绿蓝图片的灰阶定义默认值为(0, 128, 255)，若需要自定义更多的灰阶，在图片灰阶中输入数字(0-255)数字即可，多个灰阶值之间用英文逗号隔开；</span></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'.AppleSystemUIFont\';\">7. 图片高度、宽度不指定，则默认值分别为1920px/1080px；图片类型不指定，则默认为WRGB纯色类型；储存文件夹名若不指定，则默认为img；</span></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'.AppleSystemUIFont\';\">-----------------------------------------------------------------</span></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'.AppleSystemUIFont\'; font-weight:600;\">项目地址：</span><a href=\"https://github.com/FunsomMars/Timg\"><span style=\" font-family:\'.AppleSystemUIFont\'; text-decoration: underline; color:#0000ff;\">https://github.com/FunsomMars/Timg</span></a></p></body></html>"))
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'.AppleSystemUIFont\'; font-size:13pt;\">-----------------------------------------------------------------</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'.AppleSystemUIFont\'; font-size:13pt;\">1. 输入所需图片的高度（单位像素）、宽度（单位像素）、储存文件夹的名称，选择想要生成的图片类型，点击生成，就可以在指定文件夹里面生成所需图片；</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'.AppleSystemUIFont\'; font-size:13pt;\">2. 本程序支持生成的图片类型有十字对齐、白红绿蓝255灰阶单色、Crosstalk、Flicker、灰阶响应时间动图、棋盘格以及指定的多灰阶白红绿蓝图片；</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'.AppleSystemUIFont\'; font-size:13pt;\">3. 灰阶响应时间动图的默认灰阶设置为(0, 36, 63, 127, 255)五个灰阶中从低灰阶到高灰阶跳变，跳变时间间隔为0.1s，也就是说会生成0-36，0-63，0-127，0-255，36-63，36-127，36-255，63-127，63-255，127-255十种灰阶跳变动图；</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'.AppleSystemUIFont\'; font-size:13pt;\">4. 黑白棋盘格中子格子的长宽比例为图片宽高比乘以16/9；</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'.AppleSystemUIFont\'; font-size:13pt;\">5. 图片生成类型选择为crosstalk时，图片PPI的默认值为401ppi，一般来说在生成crosstalk图片时需要在图片PPI那一栏指定ppi；</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'.AppleSystemUIFont\'; font-size:13pt;\">6. 多灰阶白红绿蓝图片的灰阶定义默认值为(0, 128, 255)，若需要自定义更多的灰阶，在图片灰阶中输入数字(0-255)数字即可，多个灰阶值之间用英文逗号隔开；</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'.AppleSystemUIFont\'; font-size:13pt;\">7. 图片高度、宽度不指定，则默认值分别为1920px/1080px；图片类型不指定，则默认为WRGB纯色类型；储存文件夹名若不指定，则默认为img；</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'.AppleSystemUIFont\'; font-size:13pt;\">-----------------------------------------------------------------</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'.AppleSystemUIFont\'; font-size:13pt; font-weight:600;\">项目地址：</span><a href=\"https://github.com/FunsomMars/Timg\"><span style=\" font-family:\'.AppleSystemUIFont\'; font-size:13pt; text-decoration: underline; color:#0000ff;\">https://github.com/FunsomMars/Timg</span></a></p></body></html>"))
